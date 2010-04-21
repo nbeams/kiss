a6b4db8197de0d28083689a0ba4d2ac9eba78d08
nbeams <nbeams@ou.edu>
2010-04-20 23:53:30 -0500

    Improved drag and drop mechanism and changed New Function behavior to only allow new function definitions at the bottom of the program

--- libraries/extraGUI/src/ExtraGUIToolBar.cpp
+++ libraries/extraGUI/src/ExtraGUIToolBar.cpp
@@ -13,15 +13,13 @@ ExtraGUIToolBar::ExtraGUIToolBar(QWidget *parent) : QWidget(parent), m_ui(new Ui
     m_help = new HelpWindow();
 
     /*Connections*/
-    //connect(m_ui->newFromTemplate, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->conditionalIf, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->ifElse, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->conditionalSwitch, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->loop, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->function, SIGNAL(clicked()), this, SLOT(buttonClicked()));
     connect(m_ui->help, SIGNAL(clicked()), this, SLOT(helpRequested()));
-    connect(this, SIGNAL(newFunction(int)), this, SLOT(newFunctionFromToolBar(int)));
-    connect(this, SIGNAL(templateClicked()), this, SLOT(newFromTemplate()));
+    connect(this, SIGNAL(newFunction()), this, SLOT(newFunctionFromToolBar()));
     connect(&m_action, SIGNAL(triggered()), this, SLOT(toggleWindow()));
 
 }
@@ -155,11 +153,8 @@ void ExtraGUIToolBar::buttonClicked()
     QObject *senderObject = sender();
     QPushButton *senderButton = qobject_cast<QPushButton *>(senderObject);
 
-    if(senderObject->objectName() == "newFromTemplate")
-        emit templateClicked();
-
-    else if(senderObject->objectName() == "function")
-        emit newFunction(0);
+    if(senderObject->objectName() == "function")
+        emit newFunction();
 
     else //Regular text insertion button
     {
@@ -181,6 +176,7 @@ void ExtraGUIToolBar::toolBarDropEvent(QDropEvent *event)
    m_dropData = event->mimeData();
    int pos_y;
    int topLineShown = 0;
+   int currentTextHeight = 0;
 
    /*Check which line is currently at the top of the screen*/
    topLineShown = m_currentSourceFile->firstVisibleLine();
@@ -189,16 +185,17 @@ void ExtraGUIToolBar::toolBarDropEvent(QDropEvent *event)
      (This is to avoid problems when the drop is desired at a place currently without
         a line of text.) */
    pos_y = event->pos().y();
-   m_insertLine = topLineShown + (pos_y - 15)/16;
+   currentTextHeight = m_currentSourceFile->textHeight(0); //what pixel size is the font currently being used?
+   m_insertLine = topLineShown + (pos_y - 15)/(currentTextHeight-1);  //ugly code to convert to line number
 
    if(m_dropData->text() == "function")
-       emit newFunction(1);
+       emit newFunction();
 
    else
        m_currentSourceFile->insertAt(m_dropData->text(), m_insertLine, 0);
 }
 
-void ExtraGUIToolBar::newFunctionFromToolBar(int typeOfCall)
+void ExtraGUIToolBar::newFunctionFromToolBar()
 {
     int line = 0;
     int index = 0;
@@ -206,7 +203,6 @@ void ExtraGUIToolBar::newFunctionFromToolBar(int typeOfCall)
     int newIndex = 0;
     int mainLine = 0;
     int functionLine = 0;
-    int functionIndex = 0;
     bool textFound = true;
 
     m_funcDialog->exec();
@@ -217,13 +213,6 @@ void ExtraGUIToolBar::newFunctionFromToolBar(int typeOfCall)
     {
         if(m_funcDialog->okWasPressed())
         {
-
-            if(typeOfCall == 1) //the function is being created from a drag and drop, not a click
-                m_currentSourceFile->setCursorPosition(m_insertLine, 0);
-
-            /*Get functionLine from current cursor position*/
-            m_currentSourceFile->getCursorPosition(&functionLine, &functionIndex);
-
             /*Begin code to find insertion point for prototype*/
             /*Find location of "Function prototypes below" text*/
             if(m_currentSourceFile->findFirst("Function prototypes below", true, false, true, true, true, 0, 0, false))
@@ -273,90 +262,22 @@ void ExtraGUIToolBar::newFunctionFromToolBar(int typeOfCall)
             m_currentSourceFile->insert(*(m_funcDialog->prototype()));
 
              /*Insert function definition skeleton*/
-
-            if(functionLine <= line) //Check if the function definition will precede the prototype
+            /*Find bottom of file to insert new function*/
+            while(!(m_currentSourceFile->text(functionLine).isEmpty()))
             {
-                functionLine = line;
-                /*Find the first open line after the prototypes to place the function instead*/
-                while(m_currentSourceFile->text(functionLine) != "\n")
-                {
-                    functionLine++;
-                }
+                functionLine++;
             }
+
             m_currentSourceFile->setCursorPosition(functionLine+1, 0);
 
-            m_currentSourceFile->insert("\n{\n    //Function definition goes here.\n}\n");
+            m_currentSourceFile->insert(m_funcDialog->bodyText());
+            m_currentSourceFile->insert("\n");
             m_currentSourceFile->insert(*(m_funcDialog->prototype()));
 
         }
     }
 }
 
-void ExtraGUIToolBar::newFromTemplate()
-{
-    QString templateText;
-
-    emit(callOutsideAction());
-
-    switch(modeIndex())
-    {
-        case 0:  //Standard mode
-        {
-
-            m_currentSourceFile->insertAt("\n\n\n\n\n", 0, 0);
-            m_currentSourceFile->insertAt("/***************************************************************************/", 0, 0);
-            m_currentSourceFile->insertAt("/*********     This comment should be filled with a description    *********/", 1, 0);
-            m_currentSourceFile->insertAt("/*********         of your program and what it does.               *********/", 2, 0);
-            m_currentSourceFile->insertAt("/***************************************************************************/", 3, 0);
-
-            templateText.append("/* #defines and constants go here.*/\n\n");
-            templateText.append("/*Global variables go here (if you absolutely need them).*/\n\n");
-            templateText.append("/*Function prototypes below*/\n\n");
-            templateText.append("int main()\n{\n");
-            templateText.append("    //All variable declarations go here.\n\n");
-            templateText.append("    //This is your main function.\n\n\n");
-            templateText.append("}\n\n");
-            templateText.append("/*Declare functions here as needed.*/");
-
-            m_currentSourceFile->insert(templateText);
-
-            break;
-        }
-
-        case 1:  //Botball mode
-        {
-            m_currentSourceFile->insertAt("\n\n\n\n\n\n\n", 0, 0);
-            m_currentSourceFile->insertAt("/***************************************************************************/", 0, 0);
-            m_currentSourceFile->insertAt("/*********     This comment should be filled with a description    *********/", 1, 0);
-            m_currentSourceFile->insertAt("/*********         of your program and what it does.               *********/", 2, 0);
-            m_currentSourceFile->insertAt("/*********       This template is for Botball programs.            *********/", 3, 0);
-            m_currentSourceFile->insertAt("/*********    Make sure you have selected the CBC target.          *********/", 4, 0);
-            m_currentSourceFile->insertAt("/***************************************************************************/", 5, 0);
-
-            templateText.append("/* Libraries used in Botball are automatically included, but list any additional includes here */\n\n");
-            templateText.append("/* #defines and constants go here.*/\n\n");
-            templateText.append("/*Global variables go here (if you absolutely need them).*/\n\n");
-            templateText.append("/*Function prototypes below*/\n\n");
-            templateText.append("int main()\n{\n");
-            templateText.append("    //All variable declarations go here.\n\n");
-            templateText.append("    //This is your main function.\n\n");
-            templateText.append("    //Wait for starting light with light sensor in the_port_number\n");
-            templateText.append("        wait_for_light(the_port_number);\n\n");
-            templateText.append("    //Run your_code for the amount_of_time\n");
-            templateText.append("        run_for(amount_of_time, your_code);\n\n");
-            templateText.append("    //Make sure your motors are stopped\n");
-            templateText.append("        ao();\n");
-            templateText.append("        create_stop();\n\n");
-            templateText.append("    //Put anything below you want your robot to do after the game (e.g. play victory song).\n\n\n");
-            templateText.append("}\n\n");
-            templateText.append("/*Declare functions here as needed.*/");
-
-            m_currentSourceFile->insert(templateText);
-
-            break;
-        }
-    }
-}
 
 void ExtraGUIToolBar::setCurrentFile(SourceFile *sourceFile)
 {
